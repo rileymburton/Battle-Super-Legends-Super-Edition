d3539b634b60c11d5dcdf9a9d3c58a19e91cc240
rileymburton <35310092+rileymburton@users.noreply.github.com>
2019-01-23 14:53:32 -0800

    y

--- Battle Super Legends Super Edition/Assets/Scripts/MapGenerator.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/MapGenerator.cs
@@ -393,26 +393,22 @@ public class MapGenerator : MonoBehaviour {
 						randomDirection = Random.Range(0,4);
 					}
 					else{
-						roomExitType.RemoveAt(roomExitType.Count-2);
+						roomExitType.RemoveAt(roomExitType.Count-1);
 						if(randomDirection == 0){
 							roomExitType.Insert(roomExitType.Count-1, "ZBottom");
-							xPosition.Add(xCoordinate);
-							yPosition.Add(yCoordinate);
+							
 						}	
 						else if(randomDirection == 1){
 							roomExitType.Insert(roomExitType.Count-1, "ZLeft");
-							xPosition.Add(xCoordinate);
-							yPosition.Add(yCoordinate);
+							
 						}		
 						else if(randomDirection == 2){
 							roomExitType.Insert(roomExitType.Count-1, "ZTop");
-							xPosition.Add(xCoordinate);
-							yPosition.Add(yCoordinate);
+							
 						}	
 						else if(randomDirection == 3){
 							roomExitType.Insert(roomExitType.Count-1, "ZRight");
-							xPosition.Add(xCoordinate);
-							yPosition.Add(yCoordinate);
+							
 						}	
 						break;
 					}
