29ada5368f8ad38ee2569fbcf4f8bb677b662675
your mom <scarabbeetlearmy@gmail.com>
2019-04-26 08:46:43 -0700

    rudimentary attack data

--- Battle Super Legends Super Edition/Assets/Scripts/Player Scripts/CombatManager.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/Player Scripts/CombatManager.cs
@@ -9,8 +9,18 @@ public class CombatManager : MonoBehaviour
     public PlayerController controller;
     public PlayerMovement movement;
     public Animator animator;
-    float rollSpeed = 30;
-    int rollDuration = 30;
+
+    float rollSpeed = 5;
+    int rollDuration = 40;
+
+    float hitboxXSize = .25f;
+    float hitboxYSize = .5f;
+    float hitboxXOffset = .2f;
+    float hitboxYOffset = 0;
+    float startup = 5;
+    float active = 5;
+    float recovery = 15;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +30,33 @@ public class CombatManager : MonoBehaviour
 
     public void attack()
     {
-        
+        if (controller.m_FacingRight)
+        {
+            hitboxXOffset = Mathf.Abs(hitboxXOffset);
+        }
+        else if (!controller.m_FacingRight)
+        {
+            hitboxXOffset = -Mathf.Abs(hitboxXOffset);
+        }
+
+        for (int i = 0; i < startup; i++)
+        {
+            hitbox.enabled = false;
+        }
+        for (int i = 0; i < active; i++)
+        {
+            hitbox.enabled = true;
+        }
+        for (int i = 0; i < recovery; i++)
+        {
+            hitbox.enabled = false;
+        }
     }
 
     public void roll()
     {
+        hitbox.enabled = false;
+        
         if (controller.m_FacingRight)
         {
             rollSpeed = Mathf.Abs(rollSpeed);
@@ -34,6 +66,7 @@ public class CombatManager : MonoBehaviour
             rollSpeed = -Mathf.Abs(rollSpeed);
         }
         
+        controller.m_Rigidbody2D.velocity = Vector2.zero;
         for (int i = 0; i < rollDuration; i++)
         {
             headHitbox.enabled = false;
